2f6b8bcbd02fe63b2f2e861f59c8f0d0eb54aed1
ahjun001 <sudo.ubuntu@disroot.org>
2023-11-08 10:35:10 +0800

    stored away unused tasks, removed  ~/.vim/subdirs

--- 4.Projects/post_install/post_install.sh
+++ 4.Projects/post_install/post_install.sh
@@ -2,12 +2,25 @@
 # script to run immmediately after Fedora install. A copy should be on a usb key from where it should be run.
 
 set -euo pipefail
+set -x
 
-# will install Astrill if needed and exit to set it up, if not will pursue
-rpm -q astrill >/dev/null && rpm -i astrill-setup-linux64.rpm && exit 0
+# will install Astrill if needed and then exit to set it up, if not, will pursue
+rpm -q astrill >/dev/null || {
+    rpm -i astrill-setup-linux64.rpm && echo 'Exiting to setup Astrill ...' && exit 0
+}
 
 mkdir -p /home/perubu/Documents/Github && cd "$_"
 
+for repo in \
+    1.fedora_install_n_settings \
+    2.1.linux \
+    2.1.vim \
+    2.2.nvim \
+    2.3.podman; do
+    if [ ! -d "$repo" ]; then git clone https://github.com/ahjun001/"$repo"; fi
+done
+
+: && exit
 for repo in \
     ahjun001 \
     0.template \
