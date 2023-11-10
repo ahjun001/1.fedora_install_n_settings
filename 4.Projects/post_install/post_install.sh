13bd27e7aa0529f40719e225bbe1fe3f81cbdb76
ahjun001 <sudo.ubuntu@disroot.org>
2023-11-10 14:34:39 +0800

    finalised brave-browser install

--- 4.Projects/post_install/post_install.sh
+++ 4.Projects/post_install/post_install.sh
@@ -2,32 +2,22 @@
 # script to run immmediately after Fedora install. A copy should be on a usb key from where it should be run.
 
 set -euo pipefail
-set -x
 
 # will install Astrill if needed and then exit to set it up, if not, will pursue
 rpm -q astrill >/dev/null || {
     rpm -i astrill-setup-linux64.rpm && echo 'Exiting to setup Astrill ...' && exit 0
 }
 
+# under VPN clone public directories
 mkdir -p /home/perubu/Documents/Github && cd "$_"
 
-for repo in \
-    1.fedora_install_n_settings \
-    2.1.linux \
-    2.1.vim \
-    2.2.nvim \
-    2.3.podman; do
-    if [ ! -d "$repo" ]; then git clone https://github.com/ahjun001/"$repo"; fi
-done
-
-: && exit
 for repo in \
     ahjun001 \
     0.template \
     1.fedora_install_n_settings \
     2.1.linux \
     2.1.gh \
-    2.2.vim \
+    2.1.vim \
     2.2.nvim \
     2.2.vscode \
     2.2.bash \
@@ -35,5 +25,21 @@ for repo in \
     2.2.ffmpeg \
     2.2.asciidoctor \
     2.3.podman; do
-    if [ ! -d "$repo" ]; then git clone "$repo"; fi
+    if [ ! -d "$repo" ]; then git clone https://github.com/ahjun001/"$repo"; fi
 done
+
+read -rsn 1 -p $'\nCtrl-C to exit from VPN, or press any key to continue ...\n'
+
+# set up dnf so that optimum
+if [[ ! $(sed -n '$p' /etc/dnf/dnf.conf) == 'max_parallel_downloads=10' ]]; then
+    cat <<. | sudo tee -a /etc/dnf/dnf.conf
+fastestmirror=1
+max_parallel_downloads=10
+.
+fi
+
+sudo dnf -C install ansible
+
+cd /home/perubu/Documents/Github/1.fedora_install_n_settings/4.Projects/post_install/ansible
+
+ansible-playbook -Ki inventories/hosts playbook.yml
