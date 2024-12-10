b97c9f47ca746fdbb39de08f6152b363eb29e56c
ahjun001 <sudo.ubuntu@disroot.org>
2024-12-10 10:57:08 +0800

    typos & small changes

--- 4.Projects/post_install/post_install.sh
+++ 4.Projects/post_install/post_install.sh
@@ -7,7 +7,6 @@ set +x
 # set up dnf so that optimum
 if [[ ! $(sed -n '$p' /etc/dnf/dnf.conf) == 'max_parallel_downloads=10' ]]; then
     cat <<. | sudo tee -a /etc/dnf/dnf.conf
-fastestmirror=1 # check if that is still needed, or in this form with Fedora install files
 max_parallel_downloads=10
 .
 fi
@@ -27,25 +26,29 @@ read -rsn 1 -p $'\nCtrl-C to set VPN on, or press any key to continue ...\n'
 mkdir -p /home/perubu/Documents/Github && cd "$_"
 
 for repo in \
-    ahjun001 \
     0.template \
     1.fedora_install_n_settings \
+    1.packages_n_settings_project \
     2.1.ansible \
-    2.1.linux \
     2.1.gh \
+    2.1.linux \
     2.1.vim \
-    2.2.nvim \
-    2.2.vscode \
-    2.2.bash \
-    2.2.shellspec \
-    2.2.ffmpeg \
     2.2.asciidoctor \
-    2.3.podman; do
+    2.2.bash \
+    2.2.ffmpeg \
+    2.2.nvim \
+    2.2.shellspec \
+    2.2.vscode \
+    2.3.podman \
+    ahjun001 \
+    ; do
     if [ ! -d "$repo" ]; then git clone https://github.com/ahjun001/"$repo"; fi
 done
 
 cd /home/perubu/Documents/Github/1.fedora_install_n_settings/4.Projects/post_install/ansible
 
-ansible-playbook -Ki inventories/hosts playbook.yml
+# ansible-playbook -Ki inventories/hosts playbook.yml
+ansible-playbook -i inventories/hosts playbook.yml --ask-vault-pass -K
+
 
 gh auth setup-git
